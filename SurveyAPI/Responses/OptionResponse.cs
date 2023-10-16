6dd2becbf2add6fff97e6d34b28cdf21d2202717
Jordan Salazar <jcr.salazar@outlook.com>
2023-10-16 16:54:44 -0500

    feat: add response type

---
+++ SurveyAPI/Responses/OptionResponse.cs
@@ -0,0 +1,12 @@
+﻿using SurveyAPI.Models;
+
+namespace SurveyAPI.Responses
+{
+    public class OptionResponse
+    {
+        public int Id { get; set; }
+        public int? SurveyId { get; set; }
+        public string Alternative { get; set; }
+        public int Vote { get; set; }
+    }
+}
---
+++ SurveyAPI/Responses/SurveyResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SurveyAPI.Responses
+{
+    public class SurveyResponse
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Description { get; set; }
+    }
+}
